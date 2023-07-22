c8502ead0b4d70c42c7f43ec216cad5e89980e00
sanchayan-das_psl <sanchayan_das@persistent.com>
2023-07-22 18:03:06 +0530

    typescript popups

---
+++ Variable and data types/popup.js
@@ -0,0 +1,5 @@
+console.log('Hello');
+alert('hello');
+confirm('do you really want to do this');
+var data = prompt('please enter your name');
+console.log(data);
---
+++ Variable and data types/popup.ts
@@ -0,0 +1,6 @@
+console.log('Hello');
+alert('hello');
+confirm('do you really want to do this');
+var data = prompt('please enter your name');
+console.log(data);
+
